3819d1e2fb1dac52a0a732bb6cfae6db3dea94d2
Martin Martinez <mtz.martin@proton.me>
2023-11-10 14:49:13 -0600

    Created a seperate file view for the cards, now looks better in the Content View file, but i still think this is not a good practice :D

---
+++ FinanceDashboard/HorizontalScrollCards.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalScrollCards.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 10/11/23.
+//
+
+import SwiftUI
+
+struct HorizontalScrollCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalScrollCards()
+}
