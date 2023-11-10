9e8e65be8c42781cb30a108507d11bb0f9d29aad
Martin Martinez <mtz.martin@proton.me>
2023-11-10 15:45:50 -0600

    I added a ugly tab bar, I'll work on this soon :D

--- FinanceDashboard/ContentView.swift
+++ FinanceDashboard/ContentView.swift
@@ -8,142 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var isToggleOn = false
-    
     var body: some View {
-        VStack {
-            //Top bar
-            HStack {
-                Button {
-                    //
-                } label: {
-                    Circle()
-                        .foregroundColor(.white)
-                        .frame(width: 50)
-                        .overlay(
-                            Image(systemName: "person.fill")
-                                .foregroundColor(.black)
-                                .font(.title)
-                        )
-                }
-                
-                Text("Martin Martinez")
-                    .font(.title2)
-                    .bold()
-                
-                Spacer()
-                
-                Button {
-                    //
-                } label: {
-                    Circle()
-                        .stroke(Color.black, lineWidth: 2)
-                        .frame(width: 50)
-                        .foregroundColor(.clear)
-                        .overlay(
-                            Image(systemName: "plus")
-                                .bold()
-                                .font(.title2)
-                                .foregroundColor(.black)
-                        )
-                }
-            }
-            
-            //Body
-            RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 350, height: 150)
-                .foregroundColor(.indigo)
-                .overlay(
-                    VStack {
-                        HStack{
-                            Text("Balance")
-                                .bold()
-                                .font(.title2)
-                                .padding()
-                                .foregroundColor(.white)
-                            Spacer()
-                            Toggle("",isOn: $isToggleOn)
-                                .padding()
-                                .toggleStyle(SwitchToggleStyle(tint: .black))
-                        }
-                        HStack {
-                            if isToggleOn {
-                                Text("$2450.56")
-                                    .font(.title2)
-                                    .padding()
-                                    .foregroundColor(.white)
-                                    .bold()
-                                Spacer()
-                                
-                                Text("10/2023")
-                                    .padding()
-                                    .font(.title2)
-                                    .bold()
-                                    .foregroundColor(.white)
-                            } else {
-                                Image(systemName: "lock.fill")
-                                    .foregroundColor(.white)
-                                    .font(.title2)
-                                    .bold()
-                                
-                            }
-                        }
-
+        ZStack(alignment: .bottom) {
+            TabView {
+                FirstScreen()
+                    .tabItem {
+                        Image(systemName: "house")
+                            .foregroundColor(Color.black)
+                        Text("Home")
                     }
-                )
-            
-            HStack {
-                Text("Upcoming")
-                    .font(.title2)
-                    .bold()
-                Spacer()
-                Button {
-                    //
-                } label: {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .stroke(Color.black, lineWidth: 2)
-                        .frame(width: 100, height: 30)
-                        .foregroundColor(.clear)
-                        .overlay(
-                            Text("View All")
-                                .foregroundColor(.black)
-                                .bold()
-                        )
-                        
-                }
+                FirstScreen()
+                    .tabItem {
+                        Image(systemName: "cart")
+                        Text("Cart")
+                    }
+                FirstScreen()
+                    .tabItem {
+                        Image(systemName: "creditcard")
+                        Text("Payment")
+                    }
+                FirstScreen()
+                    .tabItem {
+                        Image(systemName: "gearshape")
+                        Text("Settings")
+                    }
             }
+            .accentColor(.black)
             
-            //Horzontal scroll body cards
-           HorizontalScrollCards()
-            
-            //Subscriptions scroll cards
-            HStack {
-                Text("Expense Summary")
-                    .font(.title2)
-                    .bold()
-                Spacer()
-                Button {
-                    //
-                } label: {
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .stroke(Color.black, lineWidth: 2)
-                        .frame(width: 100, height: 30)
-                        .foregroundColor(.clear)
-                        .overlay(
-                            Text("View All")
-                                .foregroundColor(.black)
-                                .bold()
-                        )
-                }
-            }
-            
-            ScrollView {
-                VerticalScrollCards()
-            }
-
         }
-        .padding()
     }
 }
 
---
+++ FinanceDashboard/FirstScreen.swift
@@ -0,0 +1,152 @@
+//
+//  FirstScreen.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 10/11/23.
+//
+
+import SwiftUI
+
+struct FirstScreen: View {
+    
+    @State var isToggleOn = false
+    
+    var body: some View {
+        VStack {
+            //Top bar
+            HStack {
+                Button {
+                    //
+                } label: {
+                    Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 50)
+                        .overlay(
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.black)
+                                .font(.title)
+                        )
+                }
+                
+                Text("Martin Martinez")
+                    .font(.title2)
+                    .bold()
+                
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Circle()
+                        .stroke(Color.black, lineWidth: 2)
+                        .frame(width: 50)
+                        .foregroundColor(.clear)
+                        .overlay(
+                            Image(systemName: "plus")
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.black)
+                        )
+                }
+            }
+            
+            //Body
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 350, height: 150)
+                .foregroundColor(.indigo)
+                .overlay(
+                    VStack {
+                        HStack{
+                            Text("Balance")
+                                .bold()
+                                .font(.title2)
+                                .padding()
+                                .foregroundColor(.white)
+                            Spacer()
+                            Toggle("",isOn: $isToggleOn)
+                                .padding()
+                                .toggleStyle(SwitchToggleStyle(tint: .black))
+                        }
+                        HStack {
+                            if isToggleOn {
+                                Text("$2450.56")
+                                    .font(.title2)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .bold()
+                                Spacer()
+                                
+                                Text("10/2023")
+                                    .padding()
+                                    .font(.title2)
+                                    .bold()
+                                    .foregroundColor(.white)
+                            } else {
+                                Image(systemName: "lock.fill")
+                                    .foregroundColor(.white)
+                                    .font(.title2)
+                                    .bold()
+                                
+                            }
+                        }
+
+                    }
+                )
+            
+            HStack {
+                Text("Upcoming")
+                    .font(.title2)
+                    .bold()
+                Spacer()
+                Button {
+                    //
+                } label: {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .stroke(Color.black, lineWidth: 2)
+                        .frame(width: 100, height: 30)
+                        .foregroundColor(.clear)
+                        .overlay(
+                            Text("View All")
+                                .foregroundColor(.black)
+                                .bold()
+                        )
+                        
+                }
+            }
+            
+            //Horzontal scroll body cards
+           HorizontalScrollCards()
+            
+            //Subscriptions scroll cards
+            HStack {
+                Text("Expense Summary")
+                    .font(.title2)
+                    .bold()
+                Spacer()
+                Button {
+                    //
+                } label: {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .stroke(Color.black, lineWidth: 2)
+                        .frame(width: 100, height: 30)
+                        .foregroundColor(.clear)
+                        .overlay(
+                            Text("View All")
+                                .foregroundColor(.black)
+                                .bold()
+                        )
+                }
+            }
+            
+            ScrollView {
+                VerticalScrollCards()
+            }
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FirstScreen()
+}
