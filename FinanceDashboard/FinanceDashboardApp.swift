3bfd4658b72c355d5fb382072da6a7e82547715b
Martin Martinez <mtz.martin@proton.me>
2023-11-10 13:41:06 -0600

    Initial Commit

---
+++ FinanceDashboard/FinanceDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceDashboardApp.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FinanceDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
