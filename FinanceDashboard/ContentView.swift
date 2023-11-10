d5f6a75dd09c09f57b21f15a0cbafea014cb5b5f
Martin Martinez <mtz.martin@proton.me>
2023-11-10 14:15:42 -0600

    Adding a balance card and I learned how to show content conditionally!

--- FinanceDashboard/ContentView.swift
+++ FinanceDashboard/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isToggleOn = false
+    
     var body: some View {
         VStack {
+            //Top bar
             HStack {
                 Circle()
                     .frame(width: 50)
@@ -36,8 +40,50 @@ struct ContentView: View {
                             .font(.title)
                             .bold()
                     )
-                    
             }
+            
+            //Body
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 350, height: 150)
+                .foregroundColor(.indigo)
+                .overlay(
+                    VStack {
+                        HStack{
+                            Text("Balance")
+                                .bold()
+                                .font(.title2)
+                                .padding()
+                                .foregroundColor(.white)
+                            Spacer()
+                            Toggle("",isOn: $isToggleOn)
+                                .padding()
+                                .toggleStyle(SwitchToggleStyle(tint: .black))
+                        }
+                        HStack {
+                            if isToggleOn {
+                                Text("$2450.56")
+                                    .font(.title2)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .bold()
+                                Spacer()
+                                
+                                Text("10/2023")
+                                    .padding()
+                                    .font(.title2)
+                                    .bold()
+                                    .foregroundColor(.white)
+                            } else {
+                                Image(systemName: "lock.fill")
+                                    .foregroundColor(.white)
+                                    .font(.title2)
+                                    .bold()
+                                
+                            }
+                        }
+
+                    }
+                )
             
         }
         .padding()
