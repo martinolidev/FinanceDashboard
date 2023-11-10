72a01e5da787ff513781b04dab9684426a847b8a
Martin Martinez <mtz.martin@proton.me>
2023-11-10 13:57:44 -0600

    I just created the top bar, this is just the start of this :D

--- FinanceDashboard/ContentView.swift
+++ FinanceDashboard/ContentView.swift
@@ -10,10 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Circle()
+                    .frame(width: 50)
+                    .overlay(
+                        Image(systemName: "person.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    )
+                
+                Text("Martin Martinez")
+                    .font(.title2)
+                    .bold()
+                
+                Spacer()
+                
+                Circle()
+                    .stroke(Color.black, lineWidth: 2.0)
+                    .frame(width: 50)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20)
+                            .font(.title)
+                            .bold()
+                    )
+                    
+            }
+            
         }
         .padding()
     }
