19c585a54c7bc1bd715cb056571fb22e7116bf52
Martin Martinez <mtz.martin@proton.me>
2023-11-11 11:59:44 -0600

    I created a new screen with the method payments, and It can be hidden with a toggle :D

---
+++ FinanceDashboard/WalletScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WalletScreen.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 11/11/23.
+//
+
+import SwiftUI
+
+struct WalletScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WalletScreen()
+}
