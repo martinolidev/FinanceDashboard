b7873651ec95cbc33e44e80e20045c67b728dde8
Martin Martinez <mtz.martin@proton.me>
2023-11-11 11:32:15 -0600

    I created the top bar, a little different than the previous one

--- FinanceDashboard/CartScreen.swift
+++ FinanceDashboard/CartScreen.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct CartScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            //top bar
+            HStack {
+                Button {
+                    //
+                } label: {
+                    Circle()
+                        .foregroundColor(.white)
+                        .frame(width: 50)
+                        .overlay(
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.black)
+                                .font(.title)
+                        )
+                }
+                
+                Text("Cart")
+                    .font(.title)
+                    .bold()
+                
+                Spacer()
+                
+                Image(systemName: "cart")
+                    .font(.title)
+                    .bold()
+            }
+        }
+        .padding()
     }
 }
 
