59383713c889090ef2bc753ef1ad644ad6c96620
Martin Martinez <mtz.martin@proton.me>
2023-11-11 11:31:43 -0600

    I'm starting to create the new screen :D

---
+++ FinanceDashboard/CartScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CartScreen.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 11/11/23.
+//
+
+import SwiftUI
+
+struct CartScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartScreen()
+}
