dcc3d87c8d5ad47f9214554dd336c4563e8a74b4
Martin Martinez <mtz.martin@proton.me>
2023-11-11 11:26:59 -0600

    I just created the cart screen, it looks good but it doesn't work D:

---
+++ FinanceDashboard/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  FinanceDashboard
+//
+//  Created by Martin Martinez on 11/11/23.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
